324656efea1fb93a2ac477cccaf363c6d3caeb2c
Ewooder <153860480+Ewooder@users.noreply.github.com>
2024-11-01 15:10:51 +0300

    Code writed.

---
+++ main.py
@@ -0,0 +1,9 @@
+while True:
+	x = int(input("Please enter first number: "))
+	y = int(input("Please enter second number: "))
+	print("----E_wooder(Eymen ERDOĞDU)was-here----")
+	print(x,"+",y,"=",x+y)
+	print(x,"-",y,"=",x-y)
+	print(x,"*",y,"=",x*y)
+	print(x,"/",y,"=",x/y)
+	print(x," mod ",y,"=",x%y)
